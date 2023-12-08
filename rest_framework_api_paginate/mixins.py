46d3b2199dd3d886dc939d4a59344187a1166ea2
Arnel González Rodríguez <110951872+arnelglez@users.noreply.github.com>
2023-12-08 18:26:48 -0500

    lazy correction

--- rest_framework_api_paginate/mixins.py
+++ rest_framework_api_paginate/mixins.py
@@ -7,7 +7,7 @@ from rest_framework.pagination import PageNumberPagination
 
 from django.db import models
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomPagination(PageNumberPagination):
